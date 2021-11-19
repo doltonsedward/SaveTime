fba4093c36271be27a2d3bf7b1e6743e4103ef77
D3NCyan <edwarddoltons@gmail.com>
2021-11-19 20:16:04 +0800

    install and initialize react native base

--- App.js
+++ App.js
@@ -1,14 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Import NativeBase
+import { extendTheme, NativeBaseProvider, Box } from 'native-base'
+import TextTodo from './src/components/atoms/TextTodo';
+import Home from './src/screens/Home';
+
 export default function App() {
+  const theme = extendTheme({
+    colors: {
+      primary: {
+        50: '#eeeffc',
+        100: '#cbcef6',
+        200: '#a8adf0',
+        300: '#858cea',
+        400: '#626be4',
+        500: '#3f4ade',
+        600: '#232fd1',
+        700: '#1e27ae',
+        800: '#181f8c',
+        900: '#121869',
+      },
+    }
+  })
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NativeBaseProvider theme={theme}>
       <StatusBar style="auto" />
-    </View>
-  );
+      <Box style={styles.container}>
+        <Home />
+      </Box>
+    </NativeBaseProvider>
+  )
 }
 
 const styles = StyleSheet.create({
