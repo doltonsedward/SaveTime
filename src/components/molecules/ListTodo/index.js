57ba81fcc7cc2bb866aec046174c5c7ccd1226d6
D3NCyan <edwarddoltons@gmail.com>
2021-11-21 11:48:10 +0800

    add validation if prop in ListTodo empty

--- src/components/molecules/ListTodo/index.js
+++ src/components/molecules/ListTodo/index.js
@@ -4,18 +4,26 @@ import { StyleSheet } from "react-native"
 
 import { TextTodo, StatusTodo } from "../../atoms"
 
-const ListTodo = () => {
+const ListTodo = ({ title, subtitle, statusOnPress }) => {
+    if (!title || !subtitle) {
+        return (
+            <HStack style={styles.container}>
+            </HStack>
+        )
+    }
+
     return (
         <HStack style={styles.container}>
-            <StatusTodo />
-            <TextTodo title="halo" subtitle="oke" />
+            <StatusTodo onPress={statusOnPress} />
+            <TextTodo title={title} subtitle={subtitle} />
         </HStack>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        marginLeft: 15
+        marginLeft: 15,
+        marginBottom: 5
     }
 })
 
