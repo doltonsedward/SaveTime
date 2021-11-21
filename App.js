2af9ca71598aba09a670f8f1aab90c90b4e30647
D3NCyan <edwarddoltons@gmail.com>
2021-11-21 11:47:43 +0800

    fix padding in app

--- App.js
+++ App.js
@@ -38,9 +38,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 50,
-    paddingLeft: 25,
-    paddingRight: 25,
     backgroundColor: '#fff',
   },
 });
