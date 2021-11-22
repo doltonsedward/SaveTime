b152634855d1f8c0e24048d61e8b52c0f33b71d4
D3NCyan <edwarddoltons@gmail.com>
2021-11-22 20:00:51 +0800

    install and use react-navigation in Component Container

---
+++ Container.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+// Import Navigation Container
+import { NavigationContainer } from "@react-navigation/native";
+
+// Import Stack Navigation
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator()
+
+// Import screen
+import { Home, Todos } from "./src/screens"
+
+export default function Container() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="Home"
+            >
+                <Stack.Screen 
+                    name="Home"
+                    component={Home}
+                    options={{
+                        headerShown: false
+                    }}
+                />
+                <Stack.Screen 
+                    name="Todos"
+                    component={Todos}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
