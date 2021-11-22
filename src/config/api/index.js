da0a51a630262aeaaf772bad7ca46b709b8e6c41
D3NCyan <edwarddoltons@gmail.com>
2021-11-22 19:59:51 +0800

    change base url from localhost to heroku

--- src/config/api/index.js
+++ src/config/api/index.js
@@ -1,5 +1,8 @@
 import axios from "axios"
 
+// url: http://192.168.42.75/api/v1
+// url: https://619a5e9a27827600174451fa.mockapi.io/api/v1/
+
 export const API = axios.create({
-    baseURL: "https://jsonplaceholder.typicode.com/"
+    baseURL: "https://savetime-api.herokuapp.com/api/v1"
 })
