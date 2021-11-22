b8633b08b858348ebaeeca5d522c8903de8bf128
D3NCyan <edwarddoltons@gmail.com>
2021-11-22 19:56:44 +0800

    fix Image, change the source to local image

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,24 +1,22 @@
-import React, { useState } from "react"
+import React from "react"
 import { Box, Pressable, Button, Image, Text } from "native-base"
 import { StyleSheet } from "react-native"
 
-const Home = () => {
+const Home = ({ navigation }) => {
     return (
         <Box style={styles.container}>
-            {/* image for layout */}
             <Image 
-                source={{
-                    uri: "https://i.ibb.co/FBdwQZK/woolly-done-1.png"
-                }}
+                source={require("../assets/woolly-done.png")}
                 alt="home page"
                 size={300}
                 style={{ marginBottom: 25 }}
             />
+
             <Text fontSize="xl" style={{ marginBottom: 25 }}>Safe your time now</Text>
             <Pressable color="white">
                 {({ isPressed }) => {
                     return (
-                        <Button style={{width: 150, borderRadius: 25}} bg={isPressed ? "primary.200" : "primary.500"} shadow={5}>START</Button>
+                        <Button style={{width: 150, borderRadius: 25}} bg={isPressed ? "primary.200" : "primary.500"} shadow={4} onPress={()=> navigation.navigate("Todos")}>START</Button>
                     )
                 }}
             </Pressable>
